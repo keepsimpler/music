600ee2dd22578cb23d9645fc6009f98ca6041182
feng wenfeng <fengwenfeng@gmail.com>
2024-05-19 13:15:33 +0800

    add cv

--- docs/00_directed_acyclic_graphs.py
+++ docs/00_directed_acyclic_graphs.py
@@ -1,6 +1,14 @@
 # %%
-import networkx as nx
-import numpy as np
+from pygbif import species as species
+from pygbif import occurrences as occ
+
 # %%
-print(np.random.normal())
+splist = ['Cyanocitta stelleri', 'Junco hyemalis', 'Aix sponsa',
+  'Ursus americanus', 'Pinus conorta', 'Poa annuus']
+keys = [ species.name_backbone(x)['usageKey'] for x in splist ]
+# %%
+out = [ occ.search(taxonKey = x, limit=0)['count'] for x in keys ]
+# %%
+x = dict(zip(splist, out))
+sorted(x.items(), key=lambda z:z[1], reverse=True)
 # %%
---
+++ docs/species_distribution/01_species_distribution_model.py
@@ -0,0 +1,7 @@
+# %%
+import pandas as pd
+# %%
+df = pd.read_csv('docs/species_distribution/Data_SwissBreedingBirds.csv')
+# %%
+print(df.columns)
+# %%
